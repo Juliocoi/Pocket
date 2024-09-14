bfdfdf2a05d81910cd9bad9064c1388c707fbc6b
JulioCesar <amorim.jc@gmail.com>
2024-09-14 15:24:34 -0300

    interface front-end

---
+++ web/src/components/empty-goals.tsx
@@ -0,0 +1,23 @@
+import logo from '../assets/log0-in-orbit.svg'
+import letsStart from '../assets/lets-start-illustration.svg'
+import { Plus } from 'lucide-react'
+import { Button } from './ui/button'
+import { DialogTrigger } from './ui/dialog'
+
+export function EmptyGoals() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="logo in.orbit" />
+      <img src={letsStart} alt="imagem plano de fundo" />
+      <p className="text-zinc-380 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+      </p>
+      <DialogTrigger asChild>
+        <Button>
+          <Plus className="size-4" />
+          Cadastrar meta
+        </Button>
+      </DialogTrigger>
+    </div>
+  )
+}
